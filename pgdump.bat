b20404a3fba11d6cf0becd3af3bb141f45e933d7
Lennart von Niebelschütz <gittower@niebelschuetz.com>
2015-11-19 11:54:50 +0100

    postgresql backup script windows corrected

--- pgdump.bat
+++ pgdump.bat
@@ -2,7 +2,7 @@
    set PATH_TO_POSTGRES="c:\Program Files\PostgreSQL\9.4"
    set PATH_TO_BACKUP="D:\backup"
    set DATABASE="confluence"
-   set BACKUP_NAME=focuspro_%date%.backup
+   set BACKUP_NAME=confluence_%date%.backup
    
    echo backup file name is %BACKUP_NAME%
    echo on
